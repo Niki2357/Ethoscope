c6e30dfaf3a2c05a69af5e507ea07453a7c8f3cf
Niki Hu <Niki@lawn-128-61-5-3.lawn.gatech.edu>
2025-01-31 15:40:35 -0500

    updated for names

--- UI/plots.py
+++ UI/plots.py
@@ -7,12 +7,12 @@ os.makedirs(save_dir, exist_ok=True)
 
 x_labels = ['Dec', 'Jan', "Feb", "Mar", 'Apr']
 x = list(range(len(x_labels)))
-y = [1, 1, 1, 0, 1]  # 1 = Introvert, 0 = Extrovert
+y = [0, 0, 0, 0, 1]  # 1 = Introvert, 0 = Extrovert
 
-plt.plot(x, y, linestyle='-', color='#E7880B', marker='o', label='Connected Line')
+plt.plot(x, y, linestyle='-', color='#E7880B', marker='o', label='Extroversion')
 
 plt.xticks(ticks=x, labels=x_labels) 
-plt.yticks(ticks=[0, 1], labels=["Extrovert", "Introvert"])
+plt.yticks(ticks=[0, 1], labels=["Introvert", "Extrover"])
 
 plt.xlabel('Month')
 plt.ylabel('Personality Type')
